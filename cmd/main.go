0ef978e7470a55898d9979d25e5cbcf82620f962
Austin Hill <aghman@gmail.com>
2021-07-21 23:29:38 -0700

    Added structure and breakdown of data

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"go.bug.st/serial"
 	"go.bug.st/serial/enumerator"
 )
+
+type DataLineHandler func(string) error
 
 func main() {
 
@@ -43,24 +47,92 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Read a chunk of bytes. Convert to string.
-	// If we have a newline, it indicates that the data entry is complete.
-	// If there is no newline, append to lineOfData.
-	// If there is a newline, and only one element is in the split array,
-	// append to lineOfData and submit to the handling method.
-	// If there is more than one element in the split array,
-	// take second part (or more) and add to new instance of lineOfData.
+	/* Read a chunk of bytes. Convert to string.
+	 * If we have a newline, it indicates that the data entry is complete.
+	 * If there is no newline, append to lineOfData.
+	 * If there is a newline, and only one element is in the split array,
+	 * append to lineOfData and submit to the handling method.
+	 * If there is more than one element in the split array,
+	 * take second part (or more) and add to new instance of lineOfData.
+	 */
+
 	for { //loop until we die
 		n, err := serialPort.Read(buff)
 		if err != nil {
 			log.Fatal(err)
 			break
 		}
-		if n == 0 {
+		if n == 0 { // serial port is gone
 			fmt.Println("\nEOF")
 			break
 		}
-		fmt.Printf("%v", string(buff[:n]))
+
+		stringData := string(buff[:n])
+		//fmt.Printf("%v", stringData)
+		if strings.Contains(stringData, "\n") {
+
+			newLineSplitParts := strings.Split(stringData, "\n")
+			if len(newLineSplitParts) == 2 {
+				lineOfData += newLineSplitParts[0]
+				// send to handler - TODO
+				HandleParticleData(lineOfData)
+				lineOfData = ""
+			}
+		} else {
+			lineOfData += stringData
+		}
+
 	}
 
+}
+
+const expectedParticleDataElements = 9
+
+type PanasonicSNGCJA5Data struct {
+	PM_1_0 float64
+	PM_2_5 float64
+	PM_10  float64
+	PC_0_5 int64
+	PC_1   int64
+	PC_2_5 int64
+	PC_5   int64
+	PC_7_5 int64
+	PC_10  int64
+}
+
+/* HandleParticleData processes a serial line of data from the Panasonic SN-GCJA5 Particle Sensor
+ * Data format is as follows, split by commas:
+ * 1) PM1.0
+ * 2) PM2.5
+ * 3) PM10
+ * 4) Particulate Count 0.5
+ * 5) Particulate Count 1.0
+ * 6) Particulate Count 2.5
+ * 7) Particulate Count 5.0
+ * 8) Particulate Count 7.5
+ * 9) Particulate Count 10.0
+ */
+
+func HandleParticleData(dataLine string) error {
+	fmt.Printf("Handling %s", dataLine)
+	if dataLine == "" {
+		return fmt.Errorf("dataline is empty. %s", dataLine)
+	}
+	dataComponents := strings.Split(dataLine, ",")
+	if len(dataComponents) != expectedParticleDataElements {
+		return fmt.Errorf("unexpected data format. Expected %d, received %d ", expectedParticleDataElements, len(dataComponents))
+	}
+	var currentData PanasonicSNGCJA5Data
+	currentData.PM_1_0, _ = strconv.ParseFloat(dataComponents[0], 64)
+	currentData.PM_2_5, _ = strconv.ParseFloat(dataComponents[1], 64)
+	currentData.PM_10, _ = strconv.ParseFloat(dataComponents[2], 64)
+	currentData.PC_0_5, _ = strconv.ParseInt(dataComponents[3], 0, 0)
+	currentData.PC_1, _ = strconv.ParseInt(dataComponents[4], 0, 0)
+	currentData.PC_2_5, _ = strconv.ParseInt(dataComponents[5], 0, 0)
+	currentData.PC_5, _ = strconv.ParseInt(dataComponents[6], 0, 0)
+	currentData.PC_7_5, _ = strconv.ParseInt(dataComponents[7], 0, 0)
+	currentData.PC_10, _ = strconv.ParseInt(dataComponents[8], 0, 0)
+	//fmt.Print(currentData)
+
+	return nil
 }
